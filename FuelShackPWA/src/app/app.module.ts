10d63bbddd86e476027634ddcbcb45350d744e2e
CheyenneABounds <61998423+CheyenneABounds@users.noreply.github.com>
2021-06-21 15:48:45 -0700

    added material styles to app files

--- FuelShackPWA/src/app/app.module.ts
+++ FuelShackPWA/src/app/app.module.ts
@@ -3,7 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 // Material Section
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,6 +17,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
     AppRoutingModule
   ],
   providers: [],
